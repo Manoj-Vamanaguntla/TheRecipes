6f296e9b75941cc813c8197d617df15bba515104
CH V V GOPAL RAJU KALIDINDI <S559007@NWMISSOURI.EDU>
2023-10-27 21:05:24 -0500

    Added code to signInActivity class

--- RecipesCode/app/src/main/java/com/example/therecipes/SignInActivity.java
+++ RecipesCode/app/src/main/java/com/example/therecipes/SignInActivity.java
@@ -1,4 +1,41 @@
 package com.example.therecipes;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SignInActivity {
+public class SignInActivity extends AppCompatActivity {
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button signInButton;
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        // Initialize UI components
+        usernameEditText = findViewById(R.id.usernameET);
+        passwordEditText = findViewById(R.id.passwordET);
+        signInButton = findViewById(R.id.signInBtn);
+        registerButton = findViewById(R.id.registerBtn);
+
+        // Set a click listener for the signInButton
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // Set a click listener for the registerButton
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
